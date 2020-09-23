0776ff14d559c25db6d70a9c0a93fe758fec8822
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2020-09-23 16:19:10 +0700

    Changed adapter property name and added placeholder to img

--- app/src/main/java/com/property/adapter/GridPropertyAdapter.kt
+++ app/src/main/java/com/property/adapter/GridPropertyAdapter.kt
@@ -1,5 +1,7 @@
 package com.property.adapter
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,10 +10,11 @@ import com.bumptech.glide.Glide
 import com.iss.method.CurrencyFormatter
 import com.property.BuildConfig
 import com.property.R
-import com.property.databinding.ItemListPropertyBinding
+import com.property.activity.DetailPropertyActivity
+import com.property.databinding.ItemGridPropertyBinding
 import com.property.model.DataProperty
 
-class ListPropertyAdapter: RecyclerView.Adapter<ListPropertyAdapter.ListViewHolder>() {
+class GridPropertyAdapter: RecyclerView.Adapter<GridPropertyAdapter.ListViewHolder>() {
     private val dataProperty = ArrayList<DataProperty>()
 
     fun setData(items: ArrayList<DataProperty>) {
@@ -24,12 +27,12 @@ class ListPropertyAdapter: RecyclerView.Adapter<ListPropertyAdapter.ListViewHold
     }
 
     inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val binding = ItemListPropertyBinding.bind(itemView)
+        val binding = ItemGridPropertyBinding.bind(itemView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
         val view: View =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_list_property, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.item_grid_property, parent, false)
         return ListViewHolder(view)
     }
 
@@ -40,11 +43,22 @@ class ListPropertyAdapter: RecyclerView.Adapter<ListPropertyAdapter.ListViewHold
         with(holder) {
             Glide.with(itemView.context)
                 .load(BuildConfig.BASE_IMG_PROPERTY + data.propertyImg)
+                .placeholder(R.drawable.imgplaceholder)
                 .into(binding.ivProperty)
 
             binding.tvPropertyName.text = data.propertyName
             binding.tvInstallment.text = currencyFormatter.currencyFormatter(data.installment)
             binding.tvLocation.text = data.location
+
+            itemView.setOnClickListener {
+
+                val context: Context = it!!.context
+                val intent = Intent(context, DetailPropertyActivity::class.java)
+                intent.putExtra(DetailPropertyActivity.EXTRA_PROPERTY, data)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                context.startActivity(intent)
+
+            }
         }
     }
 
